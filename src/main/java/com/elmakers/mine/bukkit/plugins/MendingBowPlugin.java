9e93bae201217e5b6508a144ae18e90e4b6c8ad8
TheBugWhisperer <aurora@elmakers.com>
2022-03-04 19:26:15 -0800

    Working Combining.

--- src/main/java/com/elmakers/mine/bukkit/plugins/MendingBowPlugin.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/MendingBowPlugin.java
@@ -1,7 +1,18 @@
 package com.elmakers.mine.bukkit.plugins;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 public class MendingBowPlugin extends JavaPlugin implements Listener {
 
@@ -18,4 +29,43 @@ public class MendingBowPlugin extends JavaPlugin implements Listener {
         // Put here anything you want to happen when the server stops
     }
 
+    @EventHandler
+    public void onAnvilPrepare(PrepareAnvilEvent event) {
+        HumanEntity player = null;
+        for (HumanEntity human : event.getViewers()) {
+            player = human;
+            break;
+        }
+        ItemStack[] itemlist = event.getInventory().getContents();
+        if (itemlist[0] == null || itemlist[1] == null ) return;
+
+        if (itemlist[0].getType() != Material.BOW) return;
+        if (itemlist[1].getType() != Material.ENCHANTED_BOOK) return;
+        ItemStack book = itemlist[1];
+        EnchantmentStorageMeta bookMeta = (EnchantmentStorageMeta)book.getItemMeta();
+
+        if (itemlist[0].containsEnchantment(Enchantment.MENDING)) {
+            if (bookMeta.hasStoredEnchant(Enchantment.ARROW_INFINITE)) {
+                event.getInventory().setRepairCost(5);
+                ItemStack result = combineItem(itemlist[0],bookMeta);
+                event.setResult(result);
+            }
+
+        }
+        if (itemlist[0].containsEnchantment(Enchantment.ARROW_INFINITE)) {
+            if (bookMeta.hasStoredEnchant(Enchantment.MENDING)) {
+                event.getInventory().setRepairCost(5);
+                ItemStack result = combineItem(itemlist[0],bookMeta);
+                event.setResult(result);
+            }
+        }
+    }
+
+    private ItemStack combineItem(ItemStack bow,EnchantmentStorageMeta bookMeta){
+        ItemStack result = bow.clone();
+        for (Map.Entry<Enchantment, Integer> entry : bookMeta.getStoredEnchants().entrySet()) {
+            result.addEnchantment(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
 }
